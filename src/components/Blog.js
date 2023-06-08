a5512bcc26dccea8287eaa2d5a57823b58e2cd31
Samuli Toppi <samuli.e.j.toppi@student.jyu.fi>
2023-06-08 17:34:25 +0300

    5.7: blogilistan frontend, step7 complete

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+ return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visible && (
+        <div>
+          {blog.url} <br/>
+          {blog.likes}<button>like</button> <br/>
+          {blog.author} <br/>
+        </div>
+      )}
+    </div>
+ )
+}
 
 export default Blog
