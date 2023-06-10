acf4542f32fa2481178b82c116b3c656c7b0aab5
Samuli Toppi <samuli.e.j.toppi@student.jyu.fi>
2023-06-10 15:27:17 +0300

    5.18: blogilistan end to end -testit, step2 complete

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,7 +1,13 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      name: 'test user',
+      username: 'testuser',
+      password: 'password'
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users/`, user)
+    cy.visit('')
   })
 
   it('Login form is shown', function() {
@@ -9,5 +15,26 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('password')
+      cy.get('#login-button').click()
+
+      cy.contains('test user logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('eionnaa')
+      cy.get('#login-button').click()
+
+      cy.should('not.contain', 'test user logged in')
+      cy.get('.error').should('contain', 'wrong username or password')
+      cy.get('.error').should('have.css', 'background-color', 'rgb(255, 0, 0)')
+      cy.get('.error').should('have.css', 'border-style', 'solid')
+    })
   })
 })
